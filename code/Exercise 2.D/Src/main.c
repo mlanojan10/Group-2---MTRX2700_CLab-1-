ef966e3c9ddbd3be53bfb32668b5b03a55f0e54e
lucdo1000 <lucdo1000@gmail.com>
2025-04-10 14:24:21 +1000

    Add files via upload

---
+++ code/Exercise 2.D/Src/main.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "serial.h"
+#include "stm32f303xc.h"
+
+int __io_putchar(int ch) {
+    SerialOutputChar((uint8_t)ch, &USART1_PORT);
+    return ch;
+}
+
+void OnLineReceived(char *string, uint32_t length) {
+    static char response[80];
+    snprintf(response, sizeof(response), "> You typed: %s\r\n> Echo: %s\r\n\r\n", string, string);
+    SerialStartTransmission(response);
+}
+
+int main(void) {
+    SerialInitialise(BAUD_115200, &USART1_PORT, NULL);
+    SerialSetReceiveCallback(&USART1_PORT, OnLineReceived);
+
+    printf("USART1 is ready. Type a line and press Enter:\r\n> ");
+
+    while (1) {
+        // UART handled via interrupts
+    }
+}
