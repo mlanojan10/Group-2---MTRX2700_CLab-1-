ec0626c9b56cc2290fa9465cfce72277cae6c6a4
zwan5837 <w1073594004@gmail.com>
2025-04-14 13:14:26 +1000

    Added exercise 2.4 code

---
+++ code/Exercise 2.4/Inc/timer.h
@@ -0,0 +1,29 @@
+#ifndef __TIMER
+#define __TIMER
+
+#include <stdint.h>
+#include "stm32f303xc.h"
+
+void enable_clocks();
+
+void initialise_board();
+
+void trigger_prescaler(TIM_TypeDef *TIM);
+
+void init_timer_module(TIM_TypeDef *TIM, uint32_t interval, void (*timer_callback)());
+
+void change_pattern();
+
+void TIM2_IRQHandler(void);
+
+void enable_timer2_interrupt();
+
+uint32_t get_timer_period(TIM_TypeDef *TIM);
+
+void set_timer_period(TIM_TypeDef *TIM, uint32_t new_period);
+
+void reset_timer(TIM_TypeDef *TIM, uint32_t new_period);
+
+void one_shot(TIM_TypeDef *TIM, uint32_t delay);
+
+#endif
