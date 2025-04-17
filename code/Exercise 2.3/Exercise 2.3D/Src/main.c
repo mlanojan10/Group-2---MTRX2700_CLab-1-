128a35b49669f426506c534bcaedf253022c3b36
lucdo1000 <lucdo1000@gmail.com>
2025-04-17 11:31:24 +1000

    Update main.c

--- code/Exercise 2.3/Exercise 2.3D/Src/main.c
+++ code/Exercise 2.3/Exercise 2.3D/Src/main.c
@@ -9,19 +9,29 @@ int __io_putchar(int ch) {
     return ch;
 }
 
+// Callback function called when a full line is received via UART
 void OnLineReceived(char *string, uint32_t length) {
     static char response[80];
-    snprintf(response, sizeof(response), "> You typed: %s\r\n> String: %s\r\n\r\n", string, string);
+
+    // Format and store a response string including the user input
+    snprintf(response, sizeof(response), 
+             "> You typed: %s\r\n> String: %s\r\n\r\n", string, string);
+
+    // Transmit the formatted response back over UART
     SerialStartTransmission(response);
 }
 
 int main(void) {
+    // Initialise USART1 with 115200 baud rate
     SerialInitialise(BAUD_115200, &USART1_PORT, NULL);
+
+    // Register the line-received callback
     SerialSetReceiveCallback(&USART1_PORT, OnLineReceived);
 
+    // Initial startup message
     printf("USART1 is ready. Type a line and press Enter:\r\n> ");
 
     while (1) {
-        // UART handled via interrupts
+        // Main loop remains idle; UART input/output handled by interrupts
     }
 }
