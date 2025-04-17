27ee6fa06c99d6a8bee3842bd1f13f6a539a0b3e
zwan5837 <w1073594004@gmail.com>
2025-04-17 12:49:43 +1000

    Update timer.c for Integration

--- code/Working Integration/Src/timer.c
+++ code/Working Integration/Src/timer.c
@@ -2,29 +2,22 @@
 #include "stm32f303xc.h"
 #include "timer.h"
 
-// status of timer operation, 0 = regular interval mode, 1 = one-shot mode
-uint8_t timer_mode = 0;
-
-// enable the clocks for desired peripherals (GPIOA, C and E)
+// Enable the clocks for desired peripherals (GPIOA, C and E)
 void timer_enable_clocks() {
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIOEEN;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 }
 
-// initialise the discovery board I/O (just outputs: inputs are selected by default)
+// Initialise the discovery board I/O (just outputs: inputs are selected by default)
 void timer_initialise_board() {
-	// get a pointer to the second half word of the MODER register (for outputs pe8-15)
+
+	// Get a pointer to the second half word of the MODER register (for outputs pe8-15)
 	uint16_t *led_output_registers = ((uint16_t *)&(GPIOE->MODER)) + 1;
-	*led_output_registers = 0x5555; //every second led is on
+	*led_output_registers = 0x5555; // All LEDs are on
 }
 
-void turn_off_led_callback(void) {
-    GPIOE->ODR &= ~0xFF00;  // Turn off PE8
-}
-
-
-// general function called to make new prescaler value take effect
-// input: desired timer number
+// General function called to make new prescaler value take effect
+// Input: desired timer number
 void trigger_prescaler(TIM_TypeDef *TIM) {
 
 	TIM->ARR = 0x01;
@@ -35,64 +28,65 @@ void trigger_prescaler(TIM_TypeDef *TIM) {
 	TIM->ARR = 0xffffffff;
 }
 
-// store a pointer to the function that is called when a timer interrupt occurs
+// Store a pointer to the function that is called when a timer interrupt occurs
 void (*on_timer_interrupt)() = 0x00;
 
 
-// initialise timer with delay time in ms to trigger a callback function regularly
-// input: desired timer number to initialise; delay time in ms; callback function
-void init_timer_module(TIM_TypeDef *TIM, uint32_t interval, void (*timer_callback)()) {
+// Initialise timer with 1ms ticks to trigger a callback function regularly
+// Input: desired timer number to initialise; callback function
+void init_timer_module(TIM_TypeDef *TIM, void (*timer_callback)()) {
 
 	TIM->CR1 |= TIM_CR1_CEN;
 	TIM->PSC = 7999;
 	trigger_prescaler(TIM);
-	TIM->CR1 = 0;
+	TIM->CR1 &= ~TIM_CR1_CEN;
 	TIM->CNT = 0;
-	TIM->ARR = interval;
-	TIM->CR1 |= TIM_CR1_CEN;
 
 	on_timer_interrupt = timer_callback;
-
 }
 
-// simple function to switch on/off every second led when called
-void change_pattern() {
-
-	GPIOE->ODR ^= 0xFF00;  // Toggle all LEDs from PE8 to PE15
-
+// Simple function to switch on/off every led when called
+void blink_all_leds() {
+	uint8_t *led_output_register = ((uint8_t*)&(GPIOE->ODR)) + 1;
+	*led_output_register ^= 0b11111111;
 }
 
-// function called when timer interrupt is raised
+// Simple function to switch on/off every second led when called
+void blink_alternate_leds() {
+	uint8_t *led_output_register = ((uint8_t*)&(GPIOE->ODR)) + 1;
+	*led_output_register ^= 0b10101010;
+}
+
+// Interrupt Service Routine
 void TIM2_IRQHandler(void) {
     // Check if the TIM2 interrupt flag is set
     if (TIM2->SR & TIM_SR_UIF) {
-        // run the callback function (make sure it is not null first)
+		// Run the callback function (make sure it is not null first)
         if (on_timer_interrupt != 0x00) {
-
         	on_timer_interrupt();
-
-        	// if timer is in one-shot mode then stop timer and reset timer to default mode
-			if (timer_mode == 1) {
-        		TIM2->CR1 = 0;
-        		timer_mode = 0;
+        	// If timer is in one-pulse mode reset timer to default mode
+			if (TIM2->SR & TIM_SR_CC1IF) {
+				// Disable capture/compare flag
+				TIM2->SR &= ~TIM_SR_CC1IF;
+        		// Disable the Capture/Compare 1 interrupt
+				TIM2->DIER &= ~TIM_DIER_CC1IE;
 			}
-
         }
         // Clear the interrupt flag (write 1 to the UIF bit to reset it)
         TIM2->SR &= ~TIM_SR_UIF;
     }
 }
 
-// intialise hardware interrupt for timer 2
+// Enable hardware interrupt for timer 2
 void enable_timer2_interrupt() {
 	// Disable the interrupts while messing around with the settings
-	//  otherwise can lead to strange behaviour
+	// Otherwise can lead to strange behaviour
 	__disable_irq();
 
 	// Enable update interrupt (UIE)
 	TIM2->DIER |= TIM_DIER_UIE;
 
-	// Tell the NVIC module that EXTI0 interrupts should be handled
+	// Tell the NVIC module that TIM2 interrupts should be handled
 	NVIC_SetPriority(TIM2_IRQn, 1);  // Set Priority
 	NVIC_EnableIRQ(TIM2_IRQn);
 
@@ -100,43 +94,43 @@ void enable_timer2_interrupt() {
 	__enable_irq();
 }
 
-// simple get function to return a specific timer's period in ms
-// assuming timers are configured to 1kHz so each count is 1ms
+// Simple get function to return a specific timer's period in ms
+// Assuming timers are configured to 1kHz so each count is 1ms
 uint32_t get_timer_period(TIM_TypeDef *TIM) {
 	return TIM->ARR;
 }
 
-// simple set function to set a specific timer's period in ms
-// assuming timers are configured to 1kHz so each count is 1ms
+// Simple set function to set a specific timer's period in ms
+// Assuming timers are configured to 1kHz so each count is 1ms
 void set_timer_period(TIM_TypeDef *TIM, uint32_t new_period) {
 	TIM->ARR = new_period;
 }
 
-// function to reset a specific timer's count with a new period in ms
-// assuming timers are configured to 1kHz so each count is ms
+// Function to reset a specific timer's count with a new period in ms
+// Assuming timers are configured to 1kHz so each count is ms
 void reset_timer(TIM_TypeDef *TIM, uint32_t new_period) {
 
-	// stop timer's count and reset count to 0
-	TIM->CR1 = 0;
+	// Stop timer's count and reset count to 0
+	TIM->CR1 &= ~TIM_CR1_CEN;
 	TIM->CNT = 0;
-	// set new period
+	// Set new period
 	set_timer_period(TIM, new_period);
-	// restart timer
+	// Restart timer
 	TIM->CR1 |= TIM_CR1_CEN;
 }
 
+// Hardware interrupt enable for converting a specific timer's operation to one-pulse mode
+// Input specific timer to be used and delay desired in ms
+void one_shot(TIM_TypeDef *TIM, uint32_t delay) {
 
-void Timer_SetPeriodic(uint32_t ms) {
-    init_timer_module(TIM2, ms, change_pattern);  // Uses toggling LED pattern
+	// Sets value for capture/compare event
+	TIM->CCR1 = delay;
+
+	// Enable one-pulse mode
+	TIM->CR1 |= TIM_CR1_OPM;
+
+	// Enable capture/compare interrupt
+	TIM->DIER |= TIM_DIER_CC1IE;
+
+	reset_timer(TIM, delay);
 }
-
-void Timer_TriggerOneShot(uint32_t ms) {
-    timer_mode = 1;
-
-    GPIOE->ODR |= 0xFF00;  // Turn ON all LEDs PE8 to PE15
-
-    on_timer_interrupt = turn_off_led_callback;  // Assign callback to turn off LED
-
-    reset_timer(TIM2, ms);  // Restart TIM2 with new delay
-}
-
